9f9ff25e109df3809531e7affd78f2182896c5e8
Rohit Jaryal <rohitjaryal@Ryans-MP.local>
2023-12-17 20:21:11 +0700

    chores: cors added

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const cors = require("cors");
+
+app.use(
+    cors({
+      origin: "*",
+    }),
+);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
